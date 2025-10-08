66f61b4e258b2b5dbfc45ced41424eed23a052ae
Eduardo <eduardoantune12@gmail.com>
2025-10-07 21:44:49 -0300

    feat: updated imports

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from blog.views import PostView, PostDetail
+from .views import PostView, PostDetail
 
 
 urlpatterns = [
--- mysite/blog/views/post_view.py
+++ mysite/blog/views/post_view.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from blog.models import Post
+from ..models import Post
 
 
 class PostView(generic.ListView):
